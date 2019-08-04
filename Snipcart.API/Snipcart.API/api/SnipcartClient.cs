df6344e064f3701cb13d23bc5a0a6774a6ddb641
shervin@itpod.co.uk <shervin@itpod.co.uk>
2019-08-04 19:11:38 +0100

    init

--- Snipcart.API/Snipcart.API/api/SnipcartClient.cs
+++ Snipcart.API/Snipcart.API/api/SnipcartClient.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Net;
-using System.Text;
 using System.Threading.Tasks;
 using Snipcart.Models;
 
--- Snipcart.API/Snipcart.API/api/http/SnipcartService.cs
+++ Snipcart.API/Snipcart.API/api/http/SnipcartService.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Snipcart
+﻿namespace Snipcart
 {
     public abstract class SnipcartService
     {
